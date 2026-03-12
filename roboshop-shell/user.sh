800b513dc81d35878b9500a1f0b98134a94dcc4c
Chandana <rebbachandana54@gmail.com>
2026-03-12 21:07:14 +0000

    s

--- roboshop-shell/user.sh
+++ roboshop-shell/user.sh
@@ -9,8 +9,6 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MONGODB_HOST="mongodb.daws-86.shop"
-SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 START_TIME=$(date +%s)
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
@@ -61,9 +59,9 @@ Validate $? "Copying user services"
 systemctl daemon-reload &>>$LOG_FILE
 Validate $? "reloading background services"
 systemctl enable user &>>$LOG_FILE
-Validate $? "enabling catlogue service"
+Validate $? "enabling user service"
 systemctl start user &>>$LOG_FILE
-Validate $? "Starting catlogue service"
+Validate $? "Starting user service"
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo -e "$G script execution completed in $TOTAL_TIME sec"
