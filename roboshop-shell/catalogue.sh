3a712f9a4628ea6a1714d0867d6d5efd1981279b
Chandana <rebbachandana54@gmail.com>
2026-03-12 20:21:04 +0000

    s

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -10,7 +10,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 MONGODB_HOST="mongodb.daws-86.shop"
-
+SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
